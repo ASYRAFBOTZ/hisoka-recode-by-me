42afd21cfd5bb6109bab80a8ea80ca4aa7d8ad50
ASYRAFBOTZ <102161935+ASYRAFBOTZ@users.noreply.github.com>
2022-07-22 14:26:19 +0700

    Update hisoka.js

--- hisoka.js
+++ hisoka.js
@@ -3140,15 +3140,15 @@ let capt = `⭔ Title: ${judul}
                             }]
                          let setbot = db.data.settings[botNumber]
                         if (setbot.templateImage) {
-                        hisoka.send5ButImg(m.chat, anu, hisoka.user.name, global.thumb, btn, global.thumb)
+                        hisoka.send5ButImg(m.chat, anu, 'Mark Botz By Asyraf', global.thumb, btn, global.thumb)
                         } else if (setbot.templateGif) {
-                        hisoka.send5ButGif(m.chat, anu, hisoka.user.name, global.visoka, btn, global.thumb)
+                        hisoka.send5ButGif(m.chat, anu, 'Mark Botz By Asyraf' global.visoka, btn, global.thumb)
                         } else if (setbot.templateVid) {
-                        hisoka.send5ButVid(m.chat, anu, hisoka.user.name, global.visoka, btn, global.thumb)
+                        hisoka.send5ButVid(m.chat, anu, 'Mark Botz By Asyraf', global.visoka, btn, global.thumb)
                         } else if (setbot.templateMsg) {
-                        hisoka.send5ButMsg(m.chat, anu, hisoka.user.name, btn)
+                        hisoka.send5ButMsg(m.chat, anu, 'Mark Botz By Asyraf', btn)
                         } else if (setbot.templateLocation) {
-                        hisoka.send5ButLoc(m.chat, anu, hisoka.user.name, global.thumb, btn)
+                        hisoka.send5ButLoc(m.chat, anu, 'Mark Botz By Asyraf', global.thumb, btn)
                         }
                      }
             break
